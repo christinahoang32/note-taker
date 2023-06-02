a7c8b1229456209d512d6cf02895d112150c2ec9
christinax32 <christina.hoang@salesforce.com>
2023-06-01 18:10:42 -0700

    adding ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlroutes = require('./routes/htmlroutes.js');
 const apiroutes = require('./routes/apiroutes.js')
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
